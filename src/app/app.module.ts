b74327f82f2c8ab31c7ddc885b60e4d254db83c6
MICSTI <stifter.michael@gmx.net>
2017-03-15 10:48:49 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { SettingsPage } from '../pages/settings/settings';
+import { MapPage } from '../pages/map/map';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    SettingsPage,
+    HomePage,
+    MapPage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    SettingsPage,
+    HomePage,
+    MapPage,
+    TabsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
